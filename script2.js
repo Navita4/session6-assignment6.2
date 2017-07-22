06230d27a397efefc8476a2e2fbdbc2165910e1f
Navita4 <navita441992@gmail.com>
2017-07-22 12:04:21 +0530

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,19 @@
+var a=5;
+var b=45;
+var c=145;
+if(a>b){
+    if (a>c){
+        console.log("Greatest no is :"+a);
+    }
+    else{
+        console.log("Greatest no is :"+c);
+    }
+}
+else{
+    if(b>c){
+        console.log("Greatest no is :"+b);
+    }
+else{
+    console.log("Greatest no is :"+c);
+}
+}
